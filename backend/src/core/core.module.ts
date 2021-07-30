840b8bc6a7dbc76e49fe6dd823fbcc86b77af800
Baney Acosta <baneymelo@gmail.com>
2021-07-30 04:19:23 -0500

    config and dependencies of graphql

---
+++ backend/src/core/core.module.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { GraphqlModule } from './graphql/graphql.module';
+
+@Module({
+  imports: [GraphqlModule]
+})
+export class CoreModule {}
